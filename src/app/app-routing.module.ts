2081c32f984d6c90ef3f6d8f6692db0e0e86ef74
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 06:26:44 +0100

    Add Campaign display to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './component/product/product.component';
 import { ViewProductComponent } from './component/product/view-product/view-product.component';
+import { CampaignComponent } from './component/campaign/campaign.component';
 
 const routes: Routes = [{ path: '', redirectTo: 'product', pathMatch: 'full' },
 { path: 'product', component: ProductComponent },
 { path: 'product/:id', component: ViewProductComponent },
+{ path: 'campaign', component: CampaignComponent },
+
 ];
 
 @NgModule({
