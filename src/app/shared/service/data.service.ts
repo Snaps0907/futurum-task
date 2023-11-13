c07b2a55ee559a7d78ae150b1e15930174e3dd95
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 04:01:15 +0100

    Add services for product

--- src/app/shared/service/data.service.ts
+++ src/app/shared/service/data.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private db : AngularFirestore) { }
+
+  addProduct(product : Product){
+    return this.db.collection("Product/").add(product);
+  }
+
+  getAllProducts(){
+    return this.db.collection("Product/").snapshotChanges();
+  }
+
+  updateProduct(product : Product){
+    return this.db.doc(`Product/${product.id}`).update(product);
+  }
+
+  deleteProduct(id : string){
+    return this.db.doc(`Product/${id}`).delete();
+  }
+
+  getProductById(id : string){
+    return this.db.doc(`Product/${id}`).valueChanges();
+  }
 }
