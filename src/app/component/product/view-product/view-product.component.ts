f5638c6e1fa2d7bbf22a152ef2acf79be77a5263
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 07:12:00 +0100

    Add view of campaigns for product

--- src/app/component/product/view-product/view-product.component.ts
+++ src/app/component/product/view-product/view-product.component.ts
@@ -3,8 +3,11 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
+import { Campaign } from 'src/app/shared/model/campaign';
 import { DataService } from 'src/app/shared/service/data.service';
+import { AddCampaignComponent } from '../../campaign/add-campaign/add-campaign.component';
 
 @Component({
   selector: 'app-view-product',
@@ -15,19 +18,26 @@ export class ViewProductComponent implements OnInit {
 
   id !: any;
   productObj !: any;
+  allCampaigns: Campaign[] = [];
+  displayedColumns: string[] = ['name', 'status', 'amount','town','action'];
+  dataSource!: MatTableDataSource<Campaign>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+  
 
   constructor(
-    private route: ActivatedRoute,
-    private dataApi: DataService,
+    private route : ActivatedRoute,
+    private dataApi : DataService,
+    private dialog : MatDialog,
+    private _snackBar : MatSnackBar
   ) {
     this.id = route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
     this.getProductById();
+    this.getAllCampaignsForProduct();
   }
 
   getProductById() {
@@ -35,5 +45,59 @@ export class ViewProductComponent implements OnInit {
       this.productObj = res;
     })
   }
+  getAllCampaignsForProduct() {
+    this.dataApi.getAllCampaigns().subscribe(res => {
+      this.allCampaigns = res.map((e : any) => {
+        const data = e.payload.doc.data();
+        if(data.product_id == this.id) {
+          data.campaign_id = e.payload.doc.id;
+          return data;
+        }
+      })
 
+      this.allCampaigns = this.allCampaigns.filter(item => item != undefined);
+      this.dataSource = new MatTableDataSource(this.allCampaigns);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  viewCampaign(row : any) {
+    window.open('/campaign/'+row.campaign_id,'_blank');
+  }
+
+  editCampaign(row : any) {
+    if(row.campaign_id == null || row.campaign_name == null) {
+      return;
+    }
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = row;
+    dialogConfig.data.title = "Edit campaign";
+    dialogConfig.data.buttonName = "Update";
+
+    console.log(dialogConfig.data);
+
+    const dialogRef = this.dialog.open(AddCampaignComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this.dataApi.updateCampaign(data);
+        this.openSnackBar("Campaign is updated successfully.", "OK")
+      }
+    })
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
 }
