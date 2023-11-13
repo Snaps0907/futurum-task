f7a45a3f94bcf22f36f29674b3d2b94788167a2f
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 05:14:30 +0100

    Update product component

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DataService } from 'src/app/shared/service/data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AddProductComponent } from './add-product/add-product.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from 'src/app/shared/model/product';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-product',
@@ -10,6 +15,13 @@ import { AddProductComponent } from './add-product/add-product.component';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit{
+
+  productsArr: any[] = [];
+  displayedColumns: string[] = ['name', 'description','action'];
+  dataSource!: MatTableDataSource<Product>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     public dialog: MatDialog,
@@ -38,7 +50,61 @@ export class ProductComponent implements OnInit{
       }
     })
   }
+
+  editProduct(row:any){
+    if(row.id == null || row.name == null){
+      return;
+    }
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = row;
+    dialogConfig.data.title = "Edit product";
+    dialogConfig.data.buttonName = "Update";
+
+    const dialogRef = this.dialog.open(AddProductComponent, dialogConfig);
+    
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this.dataApi.updateProduct(data);
+        this.openSnackBar("Product is updated successfully.", "OK")
+      }
+    })
+  }
   
+  deleteProduct(row : any) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+      title : 'Delete product',
+      productName : row.name,
+      productId : row.id
+    }
+
+    const dialogRef = this.dialog.open(DeleteProductComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(data => {
+      if(data) {
+        this.dataApi.deleteProduct(row.id);
+        this.openSnackBar("Product deleted successfully.", "OK")
+      }
+    })
+  }
+
+  getAllProducts() {
+    this.dataApi.getAllProducts().subscribe(res => {
+      this.productsArr = res.map((e : any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+
+      this.dataSource = new MatTableDataSource(this.productsArr);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
