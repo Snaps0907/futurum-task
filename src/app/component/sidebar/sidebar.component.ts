46aab29da326cd3170b2eef5bb0c9cb063a00857
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 05:58:20 +0100

    Update sidebar

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map, shareReplay } from 'rxjs';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  .pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+   ) {
+   }
+
+  ngOnInit(): void {  }
 }
