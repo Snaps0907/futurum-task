aeebdb95c79ea7623fa99c6db6655dd43df78f87
Jakub Laszczyk <jakub0907@gmail.com>
2023-11-13 06:25:42 +0100

    Add functions for Campaign

--- src/app/component/campaign/add-campaign/add-campaign.component.ts
+++ src/app/component/campaign/add-campaign/add-campaign.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DataService } from 'src/app/shared/service/data.service';
 
 @Component({
   selector: 'app-add-campaign',
   templateUrl: './add-campaign.component.html',
   styleUrls: ['./add-campaign.component.scss']
 })
-export class AddCampaignComponent {
+export class AddCampaignComponent implements OnInit{
+  form !: FormGroup;
+  title !: string;
+  buttonName !: string;
+  campaign_id !: string;
+  campaign_name !: string;
+  keywords !: string[];
+  amount !: number;
+  fund !: number;
+  status !: boolean;
+  town !: string;
+  radius !: number;
+  product_id !: string;
+  product_name !: string;
+
+  allProducts: any[] = [];
+  towns: string[] = ['Madison','Pasco','Millville','Collinsville','Bay City','New Paltz','New Harmony','West Orange','Adams','Tullahoma','East Cleveland','Wabash'];
+
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) data: any,
+    private dialogRef: MatDialogRef<AddCampaignComponent>,
+    private dataApi: DataService
+  ) {
+    this.title = data.title;
+    this.buttonName = data.buttonName;
+    this.campaign_id = data.campaign_id;
+    this.campaign_name = data.campaign_name;
+    this.keywords = data.keywords;
+    this.amount = data.amount;
+    this.fund = data.fund;
+    this.status = data.status;
+    this.town = data.town;
+    this.radius = data.radius;
+    this.product_id = data.product_id;
+    this.product_name = data.product_name;
+  }
+  ngOnInit(): void {
+    this.getAllProducts();
+    this.form = this.fb.group({
+      campaign_id: [this.campaign_id, []],
+      campaign_name: [this.campaign_name, [Validators.required]],
+      keywords: [this.keywords, [Validators.required]],
+      amount: [this.amount, [Validators.required]],
+      fund: [this.fund, [Validators.required]],
+      status: [this.status, [Validators.required]],
+      town: [this.town, []],
+      radius: [this.radius, [Validators.required, Validators.min(0)]],
+      product_id: [this.product_id, [Validators.required]],
+      product_name: [this.product_name, []]
+    })
+  }
+
+  getAllProducts() {
+    this.dataApi.getAllProducts().subscribe(res => {
+      this.allProducts = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        const product = {
+          product_name: data.name,
+          product_id: e.payload.doc.id
+        }
+        return product;
+      })
+    })
+  }
+
+  cancelRegistration() {
+    this.dialogRef.close();
+  }
+
+  async registerCampaign() {
+    this.form.value.product_name = await this.getProductName(this.form.value.product_id);
+    this.dialogRef.close(this.form.value);
+  }
+
+  getProductName(productId: string) {
+    for (let i = 0; i < this.allProducts.length; i++) {
+      if (this.allProducts[i].product_id == productId) {
+        return this.allProducts[i].product_name;
+      }
+    }
+    return "";
+  }
 
 }
--- src/app/component/campaign/campaign.component.ts
+++ src/app/component/campaign/campaign.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Campaign } from 'src/app/shared/model/campaign';
+import { Product } from 'src/app/shared/model/product';
+import { DataService } from 'src/app/shared/service/data.service';
+import { AddCampaignComponent } from './add-campaign/add-campaign.component';
 
 @Component({
   selector: 'app-campaign',
@@ -7,4 +16,88 @@ import { Component } from '@angular/core';
 })
 export class CampaignComponent {
 
+  allCampaigns: Campaign[] = [];
+  allProducts: Product[] = []
+  displayedColumns: string[] = ['name','product', 'action'];
+  dataSource!: MatTableDataSource<Campaign>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    public dialog: MatDialog,
+    private dataApi: DataService,
+    private _snackBar: MatSnackBar
+  ) { }
+  
+  ngOnInit(): void {
+    this.getAllCampaigns();
+    this.getAllProducts();
+  }
+
+  addCampaign() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = {
+      title: 'Register Campaign',
+      buttonName: 'Register'
+    }
+
+    const dialogRef = this.dialog.open(AddCampaignComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(data => {
+      if (data) {
+        this.dataApi.addCampaign(data);
+        this.openSnackBar("Registration of campaign is successful.", "OK")
+      }
+    })
+  }
+
+  getAllCampaigns() {
+    this.dataApi.getAllCampaigns().subscribe(res => {
+      this.allCampaigns = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.campaign_id = e.payload.doc.id;
+        return data;
+      })
+
+      this.dataSource = new MatTableDataSource(this.allCampaigns);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+  getAllProducts() {
+    this.dataApi.getAllProducts().subscribe(res => {
+      this.allProducts = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+    })
+  }
+
+  getProductName(id: string) {
+    let productName = '';
+    this.allProducts.forEach(element => {
+      if (element.id == id) {
+        productName = element.name;
+      }
+    });
+    return productName;
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
